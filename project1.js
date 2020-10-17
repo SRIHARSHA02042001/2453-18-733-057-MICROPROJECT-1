6b4835101495f06b4a71f4b5398b1ea22068134b
SRIHARSHA02042001 <72292389+SRIHARSHA02042001@users.noreply.github.com>
2020-10-17 16:21:59 +0530

    Add files via upload

--- project1.js
+++ project1.js
@@ -1,167 +1,122 @@
-var express=require('express');
-var app=express();
-const bodyparser=require('body-parser')
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(bodyparser.json());
+const express=require('express');
+const app=express();
+
 let server=require('./project1_server');
-let project1_config=require('./project1_config');
-let project1_middleware=require('./project1_middleware');
+let middleware=require('./project1_middleware');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 const MongoClient=require('mongodb').MongoClient;
 const url='mongodb://127.0.0.1:27017';
 const dbName='hospitalmgnt';
 let db
-MongoClient.connect(url,{ useUnifiedTopology:true },(err,client)=>{
+MongoClient.connect(url, {useUnifiedTopology:true}, (err,client)=>{
     if(err) return console.log(err);
     db=client.db(dbName);
     console.log(`connected to the database: ${url}`);
     console.log(`Database: ${dbName}`);
 });
-app.post('/createhosp',project1_middleware.checkToken,(req,res)=>{
+app.post('/addhosp', middleware.checkToken,(req,res)=>{
     var hn=req.body.hn;
     var hid=req.body.hid;
     var hloc=req.body.hloc;
     var haddr=req.body.haddr;
     var hcont=req.body.hcont;
-    var data=db.collection("hospital").insertOne({"hospitalname":hn,"hospitalid":hid, "hospitallocation":hloc, "hospitaladdress":haddr, "hospitalcontact":hcont});
-    res.send(hn+" "+hid+" "+hloc+" "+haddr+" "+hcont+"inserted into hospital details");
-});
-app.post('/createvent',project1_middleware.checkToken,(req,res)=>{
+    db.collection("hospital").insertOne({"hospname":hn,"hospid":hid, "hosplocation":hloc, "hospaddress":haddr, "hospcontact":hcont});
+    res.send("1 hospital details inserted successfully.");
+})
+app.post('/addvent', middleware.checkToken, (req,res)=>{
     var hid=req.body.hid;
     var vtid=req.body.vtid;
     var vs=req.body.vs;
     var hn=req.body.hn;
-    var data=db.collection("ventilator").insertOne({"hospitalid":hid, "ventilatorid":vtid, "ventilatorstatus": vs, "hospitalname":hn});
-    res.send(vtid+" "+hid+" "+hn+" "+vs+"inserted into ventilator details");
+    var data={hospid:hid, ventid:vtid, ventstat: vs, hospname:hn};
+    db.collection("ventilator").insertOne(data,function(err,result){
+        res.json("1 ventilator details inserted successfully.");
+    });
 });
 
-app.get('/hospdet',project1_middleware.checkToken,(function(req,res){
-    if(req.query.hid==undefined && req.query.hn==undefined && req.query.hloc==undefined && req.query.hcont==undefined)
-    {
+app.get('/hospdet', middleware.checkToken,function(req,res){
     db.collection("hospital").find().toArray(function(err,result){
         if(err) throw err;
         res.send(result);
         });
-    }
-    else if(req.query.hn==undefined && req.query.hloc==undefined && req.query.hcont==undefined)
-    {
-        var hid=req.query.hid;
-        db.collection("hospital").find({"hospitalid":hid}).toArray(function(err,result){
-            if(err) throw err;
-            res.json(result);
-        });
-    }
-    else if(req.query.hid==undefined && req.query.hloc==undefined && req.query.hcont==undefined)
-    {
-        var hn=req.query.hn;
-        db.collection("hospital").find({"hospitalname":new RegExp(hn,'i')}).toArray(function(err,result){
+    });
+app.post('/hospdetbyid', middleware.checkToken,function(req,res){
+        var hid=req.body.hid;
+        db.collection("hospital").find({"hospid":req.body.hid}).toArray(function(err,result){
             if(err) throw err;
             res.send(result);
-        });
-    }
-    else if(req.query.hn==undefined && req.query.hid==undefined && req.query.hcont==undefined)
-    {
-        var hloc=req.query.hloc;
-        db.collection("hospital").find({"hospitallocation":new RegExp(hloc,'i')}).toArray(function(err,result){
-            if(err) throw err;
-            res.send(result);
-        });
-    }
-    /*else if(req.query.hid==undefined && req.query.hcont==undefined)
-    {
-        db.collection("hospital").find().toArray(function(err,result){
-            if(err) throw err;
-            res.send(result);
-            var ans="";
-            var hn=req.query.hn;
-            var hloc=req.query.hloc;
-            for(var i=0;i<result.length;i++)
-            {
-                var obj=result[i];
-                if(obj.hospname==hn && obj.hosplocation==hloc){
-                ans+="hospital name:"+obj.hospname+"<br>hospital ID:"+obj.hospid+"<br>Location:"+obj.hosplocation+"<br>Address:"+obj.hospaddress+"<br>Contact Info.:"+obj.hospcontact+"<br>-----------------------------<br>";
-            }}
-            res.send(ans);
             });
-    }*/
-    else if(req.query.hcont!=undefined)
-    {
-        var hn=req.query.hn;
-        db.collection("hospital").find({"hospitalcontact":hcont}).toArray(function(err,result){
+    });
+app.post('/hospdetbynm', middleware.checkToken,function(req,res){
+        var hn=req.body.hn;
+        db.collection("hospital").find({"hospname":new RegExp(hn,'i')}).toArray(function(err,result){
             if(err) throw err;
             res.send(result);
-        });
-    }
-}));
-app.get('/ventdet',project1_middleware.checkToken,function(req,res){
-    if(req.query.vtid==undefined && req.query.vs==undefined && req.query.hn==undefined)
-    {
+            });
+    });
+app.post('/hospdetbyloc', middleware.checkToken,function(req,res){
+        var hloc=req.body.hloc;
+        db.collection("hospital").find({"hosplocation":new RegExp(hloc,'i')}).toArray(function(err,result){
+            if(err) throw err;
+            res.send(result);
+            });
+    });
+app.post('/hospdetbycont', middleware.checkToken,function(req,res){
+        var hcont=req.body.hcont;
+        db.collection("hospital").find({"hospcontact":hcont}).toArray(function(err,result){
+            if(err) throw err;
+            res.send(result);
+            });
+    });
+
+app.get('/ventdet', middleware.checkToken,function(req,res){
     db.collection("ventilator").find().toArray(function(err,result){
         if(err) throw err;
         res.send(result);
         });
-    }
-    else if(req.query.vs==undefined && req.query.hn==undefined)
-    {
-        var vtid=req.query.vtid;
-        db.collection("ventilator").find({"ventilatorid":vtid}).toArray(function(err,result){
+    });
+app.post('/ventdetbyid', middleware.checkToken,function(req,res){
+        var vtid=req.body.vtid;
+        db.collection("ventilator").find({"ventid":vtid}).toArray(function(err,result){
             if(err) throw err;
             res.send(result);
-        });
-
-    }
-    else if(req.query.vtid==undefined && req.query.hn==undefined)
-    {
-        var vs=req.query.vs;
-        db.collection("ventilator").find({"ventilatorstatus":vs}).toArray(function(err,result){
-            if(err) throw err;
-            res.send(result);
-        });
-    }
-    else if(req.query.vtid==undefined && req.query.vs==undefined)
-    {
-        var hn=req.query.hn;
-        db.collection("ventilator").find({"hospitalname":new RegExp(hloc,'i')}).toArray(function(err,result){
-            if(err) throw err;
-            res.send(result);
-        });
-    }
-    /*else if(req.query.vtid==undefined)
-    {
-        db.collection("ventilator").find().toArray(function(err,result){
-            if(err) throw err;
-            //res.send(result);
-            var ans="";
-            var vs=req.query.vs;
-            var hn=req.query.hn;
-            for(var i=0;i<result.length;i++)
-            {
-                var obj=result[i];
-                if(obj.ventstat==vs && obj.hospname==hn){
-                ans+="hospital ID:"+obj.hospid+"<br>Ventilator ID:"+obj.ventid+"<br>Ventilator Status:"+obj.ventstat+"<br>Hospital Name:"+obj.hospname+"<br>-----------------------------<br>";
-            }}
-            res.send(ans);
             });
-    }*/
-});
 
-app.delete('/delvent',project1_middleware.checkToken,(req,res)=>{
-    var vtid=req.query.vtid;
+    });
+app.post('/ventdetbystat', middleware.checkToken,function(req,res){
+        var vs=req.body.vs;
+        db.collection("ventilator").find({"ventstat":vs}).toArray(function(err,result){
+            if(err) throw err;
+            res.send(result);
+            });
+    });
+app.post('/ventdetbynm', middleware.checkToken,function(req,res){
+        var hn=req.body.hn;
+        db.collection("ventilator").find({"hospname":new RegExp(hn,'i')}).toArray(function(err,result){
+            if(err) throw err;
+            res.send(result);
+            });
+    });
+
+app.delete('/delvent', middleware.checkToken,(req,res)=>{
+    var vtid=req.body.vtid;
     try{
-        db.collection("ventilator").deleteMany({"ventilatorid":vtid});
+        db.collection("ventilator").deleteMany({"ventid":vtid});
     }
-    catch(e){console.log(e)}
+    catch(e){res.send("Details not found for "+vtid+" to delete it!");console.log(e);}
     res.send("Details of Ventilator(s) having ventilator ID as "+vtid+" has been succesfully deleted");
 });
 
-app.put('/updatevent',project1_middleware.checkToken,(req,res)=>{
-    if(req.query.vtid==undefined && req.query.hn!=undefined)
-    {
-        db.collection("ventilator").updateMany({"hospitalname":req.query.hn},[{$set:{"ventilatorstatus":req.query.vs}}]);
-    }
-    else if(req.query.vtid!=undefined)
-    {
-        db.collection("ventilator").updateMany({"ventilatorid":req.query.vtid},[{$set:{"ventilatorstatus":req.query.vs}}]);
-    }
+app.post('/updateventbynm', middleware.checkToken,(req,res)=>{
+    db.collection("ventilator").updateMany({"hospname":req.body.hn},[{$set:{"ventstat":req.body.vs}}]);
+    res.send("Details of Ventilator(s) has been succesfully Updated");
+});
+app.post('/updateventbyid', middleware.checkToken,(req,res)=>{
+        db.collection("ventilator").updateMany({"ventid":req.body.vtid},[{$set:{"ventstat":req.body.vs}}]);
     res.send("Details of Ventilator(s) has been succesfully Updated");
 });
 
